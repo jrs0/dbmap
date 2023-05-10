b467632fa3dfd88d6302a7cf35425ae31163b9e1
John Scott <john.scott17@nhs.net>
2023-05-10 12:03:48 +0100

    Made some progress on the mortality and clinical code components

--- pages/record.tsx
+++ pages/record.tsx
@@ -9,11 +9,70 @@ interface Timestamp {
     readable: string;
 }
 
+interface ClinicalCode {
+    name: string
+    docs: string
+    groups: string[]
+}
+
+interface Mortality {
+    alive: boolean
+    date_of_death: Timestamp
+    cause_of_death: ClinicalCode
+}
+
 interface AcsRecord {
     nhs_number: integer;
     age_at_index: integer;
     date_of_index: Timestamp;
     presentation: string;
+}
+
+function Date({ timestamp }: { Timestamp }) {
+    return <span>
+    {timestamp.readable}
+    </span>
+}
+
+function PatientInfo({ record }: { AcsRecord }) {
+    return <div>
+	<b>Patient Information</b>
+	<div>NHS number: {record.nhs_number}</div>
+	<div>Age at index: {record.age_at_index}</div>
+	<div>Date of index: <Date timestamp = {record.date_of_index}/></div>
+	<div>Presentation: {record.presentation}</div>
+	<div>Inclusion trigger: {record.inclusion_trigger}</div>
+    </div>
+}
+
+function ClinicalCodeComp({ clinical_code }: { ClinicalCode }) {
+    return <span>
+	<span><b>{clinical_code.name}</b></span>
+	<span>{clinical_code.docs}</span>
+	({
+	    clinical_code.groups.map(group => <span>
+		{group},
+	    </span>)
+	})
+    </span>
+}
+
+function Mortality({ mortality }: { Mortality }) {
+
+    let alive = "Alive"
+    if (mortality.alive) {
+	return <div>
+	    <b>Mortality</b>: Alive
+	</div>
+    } else {
+	return <div>
+	    <b>Mortality</b>:
+	       <div>Date of death:
+		   <div><Date timestamp = {mortality.date_of_death} /></div>
+		   <div><ClinicalCodeComp clinical_code ={mortality.cause_of_death} /></div>
+	       </div>
+	</div>
+    }
 }
 
 export default function Home() {
@@ -45,13 +104,8 @@ export default function Home() {
     } else {
 	return <div> {
 	    acs_records.map(record => <div>
-		<div>
-		    NHS number: {record.nhs_number}
-		</div><div>
-		    Age at index: {record.age_at_index}
-		</div><div>
-		    Date of index: {record.date_of_index.readable}
-		</div>
+		<PatientInfo record = {record} />
+		<Mortality mortality = {record.mortality} />
 	    </div>)
 	} </div>
 
